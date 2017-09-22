aa3e50317a390853de1fbd21925e2ad41b07341c
ningrulin <ningrulin@meetsoon.cn>
2017-09-22 14:55:27 +0800

    move mysql to constant

--- constant.py
+++ constant.py
@@ -1,5 +1,5 @@
 
-
+import pymysql.cursors
 ###URLS
 WS_URL = "ws://mxcloud.meetsoon.net/push/subscribe"
 HTTP_CAS = "http://cas.meetsoon.net"
@@ -12,6 +12,51 @@ HTTP_CAS_URL = "cas.meetsoon.net"
 # HTTP_URL= "116.52.253.210:9999"
 
 
+###SQLconfig
+configYNsifa = {
+    'host': '116.52.253.210',
+    'port': 3306,
+    'user': 'root',
+    'password': 'QWerASdf12#$',
+    'db': 'mxsuser',
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor,
+}
+configPublic = {
+    'host': 'rds94shy9735xhk329mvpublic.mysql.rds.aliyuncs.com',
+    'port': 3306,
+    'user': 'mxsuser',
+    'password': 'mxspassword',
+    'db': 'mxsuser',
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor,
+}
+
+# Connect to the database
+connection = pymysql.connect(**configPublic)
+#connection = pymysql.connect(**configYNsifa)
+
+
+# do sql
+
+def get_remote_id(remote_sn):
+
+    try:
+        with connection.cursor() as cursor:
+            # search
+            sql = 'SELECT userEntity_userID FROM mxsuser.account WHERE NAME =' + remote_sn
+            cursor.execute(sql)
+            # get result
+            result = cursor.fetchone()
+            userid = result['userEntity_userID']
+            print (userid)
+        # commit
+        connection.commit()
+
+    finally:
+        connection.close()
+
+    return userid
 
 
 
